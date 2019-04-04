627040c5cdef112eb0ce5b4763613282db0ee031
Johnson Joseph <jhonsonj88@gmail.com>
2019-04-04 15:24:49 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,4 +1,4 @@
-class add_birthdate_type < ActiveRecord::Migration[5.1]
+class Add_birthdate_type < ActiveRecord::Migration[5.1]
   
   def change
     change_column :students, :birthdate, :datetime
