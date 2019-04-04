db642c35ce6bad1a219714b59b4467e43a0cdeca
Johnson Joseph <jhonsonj88@gmail.com>
2019-04-04 15:08:19 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -7,7 +7,7 @@ class CreateStudents < ActiveRecord::Migration[5.1]
   end 
   
   def change
-    create_table :CreateStudents do |t|
+    create_table :students do |t|
       t.string :name
     end 
   end 
